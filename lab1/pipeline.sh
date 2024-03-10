ebdffb87ee55bc0b395951204c1e267955bfd9da
Vlad Lugovykh <71728038+GachiSlave@users.noreply.github.com>
2024-03-10 19:38:21 +0500

    Update pipeline.sh

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pip install -r lab1/requirements.txt
-python3 lab1/data_creation.py
-python3 lab1/model_preparation.py
-python3 lab1/model_preprocessing.py
-python3 lab1/model_testing.py
+pip install -r requirements.txt
+python3 data_creation.py
+python3 model_preparation.py
+python3 model_preprocessing.py
+python3 model_testing.py
